e7101be3ddfd1bc8ac162fa778c09f4d1d1cb491
murpheycm <cmmurphey@gmail.com>
2023-10-23 20:44:21 -0400

    Updated models index.js

---
+++ server/models/index.js
@@ -0,0 +1,6 @@
+const User = require('./User');
+const Comment = require('./Comment');
+const Cocktail = require('./Cocktail');
+const Reaction = require('./Reaction');
+
+module.exports = { User, Comment, Cocktail, Reaction };
